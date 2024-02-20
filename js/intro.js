9ec887b0c4150a14603cfe5b1278f52c47e64a64
MiaMoreLuna <157151944+MiaMoreLuna@users.noreply.github.com>
2024-02-20 01:36:35 -0800

    Box color change.
    Adjusting box color on click.

--- js/intro.js
+++ js/intro.js
@@ -5,3 +5,25 @@ $("#Secret-Text").hide();
 $("#toggle-text").click (function()  {
     $("#Secret-Text").toggle();
 });
+
+$("#A-box").hide();
+
+$("#toggle-text").click(function() {
+    $("#A-box").toggle();
+
+});
+
+
+    $ ("#My-Name").click (function() {
+        $ ("#A-box").css("background-color","pink");
+
+    });
+
+
+    $("#A-box").click (function() {
+        $("#A-box").css ("Width 500px");
+    })
+
+
+
+
